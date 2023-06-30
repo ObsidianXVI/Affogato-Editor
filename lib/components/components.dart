fdd371fab1472dd09849001f2fb04e49fc2da656
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-06-30 16:53:29 +0800

    add line numbers stash this commit, about to experiment with a diff model

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -3,7 +3,7 @@ library affogato.components;
 import 'package:affogato/style/style.dart';
 import 'package:flutter/material.dart';
 
-abstract class AffogatoComponent extends StatelessWidget {
+abstract class AffogatoComponent extends StatefulWidget {
   final AffogatoWidgetTheme theme;
 
   const AffogatoComponent({
@@ -17,24 +17,81 @@ class EditorComponent extends AffogatoComponent {
   });
 
   @override
+  State<StatefulWidget> createState() => EditorState();
+}
+
+class EditorState extends State<EditorComponent> {
+  final TextEditingController editingController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
+    final int lineCount = editingController.text.split('\n').length;
     return Container(
       width: double.infinity,
       height: double.infinity,
       decoration: BoxDecoration(
-        color: theme.editorBackground,
+        color: widget.theme.editorBackground,
       ),
-      child: TextField(
-        decoration: InputDecoration(
-          disabledBorder: InputBorder.none,
-          border: InputBorder.none,
-          focusedBorder: InputBorder.none,
-          isDense: false,
-        ),
-        enabled: true,
-        expands: true,
-        maxLines: null,
-        minLines: null,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 50,
+            height: double.infinity,
+            child: Column(children: [
+              const SizedBox(height: 7.5),
+              ...List<Widget>.generate(lineCount, (int i) {
+                return Center(
+                  child: Container(
+                    width: 40,
+                    height: 24,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: 2,
+                          right: 5,
+                          child: Text(
+                            i.toString(),
+                            style: TextStyle(
+                              fontSize: 18,
+                              height: 1.4,
+                              fontFamily: 'DMMono',
+                              color: widget.theme.primaryColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            ]),
+          ),
+          Expanded(
+            child: SizedBox(
+              child: TextField(
+                controller: editingController,
+                onChanged: (String text) {
+                  setState(() {});
+                },
+                style: TextStyle(
+                  fontSize: 18,
+                  height: 1.4,
+                  fontFamily: 'DMMono',
+                  color: widget.theme.primaryColor,
+                ),
+                decoration: const InputDecoration(
+                  disabledBorder: InputBorder.none,
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  isDense: false,
+                ),
+                enabled: true,
+                expands: true,
+                maxLines: null,
+                minLines: null,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
