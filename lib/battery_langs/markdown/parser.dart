755bd10da3cdc27c64ff1f150e57d4ae65fb666a
ObsidianXVI <siddharth.chitikela@gmail.com>
2024-06-02 16:34:52 +0800

    more markdown parsing functions

--- lib/battery_langs/markdown/parser.dart
+++ lib/battery_langs/markdown/parser.dart
@@ -17,9 +17,26 @@ class MarkdownParser extends Parser {
 
   ASTNode parseStatement() {
     if (cursor.current.tokenType == const TokenType.numberSign()) {
-      return cursor.peek().first.tokenType == const TokenType.space()
-          ? parseHeaderOne()
-          : parseBody();
+      switch (cursor.peek().first.tokenType) {
+        case const TokenType.space():
+          return parseHeaderOne();
+        case const TokenType.numberSign():
+          final nextTwoTokens = cursor.peek(lookAhead: 2);
+          if (nextTwoTokens[1].tokenType == const TokenType.numberSign()) {
+            final nextThreeTokens = cursor.peek(lookAhead: 3);
+            if (nextThreeTokens[2].tokenType == const TokenType.space()) {
+              return parseHeaderThree();
+            } else {
+              return parseBody();
+            }
+          } else if (nextTwoTokens[1].tokenType == const TokenType.space()) {
+            return parseHeaderTwo();
+          } else {
+            return parseBody();
+          }
+        default:
+          return parseBody();
+      }
     } else {
       return parseBody();
     }
@@ -27,6 +44,40 @@ class MarkdownParser extends Parser {
 
   HeaderOneNode parseHeaderOne() {
     final node = HeaderOneNode()
+      ..tokens.addAll([cursor.current, cursor.advance()]);
+    cursor.advance();
+    while (cursor.current.tokenType != const TokenType.newline() &&
+        !cursor.reachedEOF) {
+      node.tokens.add(cursor.current);
+      cursor.advance();
+    }
+
+    if (cursor.current.tokenType == const TokenType.newline()) {
+      node.tokens.add(cursor.current);
+      cursor.skip();
+    }
+    return node;
+  }
+
+  HeaderTwoNode parseHeaderTwo() {
+    final node = HeaderTwoNode()
+      ..tokens.addAll([cursor.current, cursor.advance()]);
+    cursor.advance();
+    while (cursor.current.tokenType != const TokenType.newline() &&
+        !cursor.reachedEOF) {
+      node.tokens.add(cursor.current);
+      cursor.advance();
+    }
+
+    if (cursor.current.tokenType == const TokenType.newline()) {
+      node.tokens.add(cursor.current);
+      cursor.skip();
+    }
+    return node;
+  }
+
+  HeaderThreeNode parseHeaderThree() {
+    final node = HeaderThreeNode()
       ..tokens.addAll([cursor.current, cursor.advance()]);
     cursor.advance();
     while (cursor.current.tokenType != const TokenType.newline() &&
@@ -58,7 +109,16 @@ class MarkdownParser extends Parser {
 }
 
 class HeaderOneNode extends ASTNode {
-  HeaderOneNode() : super('HeaderOneNode', scopes: ['heading']);
+  HeaderOneNode() : super('HeaderOneNode', scopes: ['heading', 'heading.one']);
+}
+
+class HeaderTwoNode extends ASTNode {
+  HeaderTwoNode() : super('HeaderTwoNode', scopes: ['heading', 'heading.two']);
+}
+
+class HeaderThreeNode extends ASTNode {
+  HeaderThreeNode()
+      : super('HeaderThreeNode', scopes: ['heading', 'heading.three']);
 }
 
 class BodyNode extends ASTNode {
--- lib/battery_themes/affogato_classic/synax_highlighter.dart
+++ lib/battery_themes/affogato_classic/synax_highlighter.dart
@@ -4,12 +4,24 @@ class AffogatoSyntaxHighlighter extends SyntaxHighlighter<AffogatoRenderToken> {
   @override
   List<AffogatoRenderToken> createRenderTokens(AST ast) {
     final List<AffogatoRenderToken> renderTokens = [];
-
     for (final node in ast.nodes) {
-      if (node.scopes.contains('heading')) {
+      if (node.scopes.contains('heading.two') ||
+          node.scopes.contains('heading.three')) {
         renderTokens.add(HeaderBoldRenderToken(node));
+      } else if (node.scopes.contains('heading.one')) {
+        renderTokens.add(HeaderOneRenderToken(node));
       } else if (node.scopes.contains('string')) {
-        renderTokens.add(DefaultPlainTextRenderToken(node));
+        renderTokens.add(StringRenderToken(node));
+      } else if (node.scopes.contains('number')) {
+        renderTokens.add(NumberRenderToken(node));
+      } else if (node.scopes.contains('keyword')) {
+        renderTokens.add(KeywordRenderToken(node));
+      } else if (node.scopes.contains('class')) {
+        renderTokens.add(ClassRenderToken(node));
+      } else if (node.scopes.contains('comment')) {
+        renderTokens.add(CommentRenderToken(node));
+      } else if (node.scopes.contains('built_in')) {
+        renderTokens.add(BuiltInRenderToken(node));
       } else {
         renderTokens.add(DefaultPlainTextRenderToken(node));
       }
